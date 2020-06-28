1a0e49ab052c05d9e580f4df5d5edf409d846e04
secondtonone1 <secondtonone1@163.com>
2020-06-28 11:45:01 +0800

    modified:   ../example/wstest/wstest.go

--- example/wstest/wstest.go
+++ example/wstest/wstest.go
@@ -14,14 +14,14 @@ import (
 
 var gLocker sync.Mutex    //全局锁
 var gCondition *sync.Cond //全局条件变量
-
+/*
 var origin = "http://81.68.86.146:9527/"
 var url = "ws://81.68.86.146:9527/wsmsg"
+*/
 
-/*
 var origin = "http://127.0.0.1:9527/"
 var url = "ws://127.0.0.1:9527/wsmsg"
-*/
+
 
 //错误处理函数
 func checkErr(err error, extra string) bool {
